9b41001c209f13d803adcbfb6e62dc78f2e17715
Vinícius Moura <viniciusmouradacosta@gmail.com>
2025-02-17 12:42:28 -0400

    subindo ultimas alterações para o Git

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -2,5 +2,21 @@ $(document).ready(function() {
     $('#mobile_btn').on('click', function () {
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find('i').toggleClass('fa-x');
-    })
-})
+    });
+
+    const sections = $('section');
+    const navItems = $('.nav-item');
+
+    $(window).on('scroll', function () {
+        const header = $('header');
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+        
+        let activeSectionIndex = 0;
+
+        if (scrollPosition <= 0) {
+            header.css('box-shadow', 'none');
+        } else {
+            header.css('box-shadow', '0px 0px 12px 4px rgba(0, 0, 0, 0.1)');
+        }
+    });
+});
